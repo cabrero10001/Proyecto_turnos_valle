dce65a8d5d37de84f8ba1af149abba829db03c26
Santiago Salazar <ssalazarb918@gmail.com>
2025-02-20 16:51:11 -0500

    se actualizó App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,8 +4,9 @@ function App() {
 
   return (
     <>
-      <h1>Pruebas de pagina</h1>
-      <p>Prueba para repositorio</p>
+      
+      <h1>Hola mundo</h1>
+      <p>hola perra</p>
     </>
   )
 }
