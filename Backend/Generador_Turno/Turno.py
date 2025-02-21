84f0d089beb6d400e3c137f7be8c7a7eb1999693
Santiago Salazar <ssalazarb918@gmail.com>
2025-02-21 17:18:43 -0500

    practica de flask

--- Backend/Generador_Turno/Turno.py
+++ Backend/Generador_Turno/Turno.py
@@ -1,25 +1,9 @@
-from flask import Flask, jsonify, request
-from flask_cors import CORS 
+from flask import Flask
 
 app = Flask(__name__)
-CORS(app) 
 
-turnos = []
-contador = 1
+@app.route('/')
+def index():
+    return 'Hello, World!'
 
-@app.route('/generar-turno', methods=['POST'])
-def generar_turno():
-    global contador
-    data = request.get_json()
-    especialidad = data.get('especialidad', 'General')
-    turno = {"id": contador, "especialidad": especialidad, "estado": "En espera"}
-    turnos.append(turno)
-    contador += 1
-    return jsonify(turno), 201
-
-@app.route('/turnos', methods=['GET'])
-def obtener_turnos():
-    return jsonify(turnos)
-
-if __name__ == '__main__':
-    app.run(debug=True)
+app.run(port=5000, debug=True)
