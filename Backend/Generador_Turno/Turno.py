209371067b5af6e7cef8b96185d76613122d5d67
Santiago Salazar <ssalazarb918@gmail.com>
2025-02-21 21:59:18 -0500

    Se creo el .py de la conexión a ala base de datos, se configuro el formulario y falta la implementacion de la bd en turno.py

--- Backend/Generador_Turno/Turno.py
+++ Backend/Generador_Turno/Turno.py
@@ -1,9 +1,12 @@
 from flask import Flask
+from ConexionDB import Conexion
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    conexion = Conexion()
     return 'Hello, World!'
 
-app.run(port=5000, debug=True)
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
--- frontend/src/components/form.jsx
+++ frontend/src/components/form.jsx
@@ -4,8 +4,28 @@ export default function form () {
     return (
         <div>
             <form action="/form" >
-                <label>Enter your name:</label>
-                <input type="text" name="name" />
+                <label>Tipo de documento:</label>
+                <select name="Documento" id="Documento">
+                    <option value="Cédula de ciudadania">Cédula de ciudadania</option>
+                    <option value="Tarjeta de identidad">Tarjeta de identidad</option>
+                    <option value="Cédula de extrangeria">Cédula de extranjería</option>
+                    <option value="Pasaporte">Pasaporte</option>
+                    <option value="Registro Civil">Registro Civil</option>
+                    <option value="Permiso especial de permanencia">Permiso especial de permanencia</option>
+                </select>
+                <label>No. Cédula:</label>
+                <input type="text" name="cedula" placeholder="Cédula" />
+                <label>Nombre:</label>
+                <input type="text" name="nombre" placeholder="Nombre" />
+                <label>Seleccione su condición:</label>
+                <select name="Condicion" id="condicion">
+                    <option value="Ninguna">Ninguna</option>
+                    <option value="Adulto Mayor">Adulto Mayor</option>
+                    <option value="Mujer Embarazada-con niño en brazos">Mujer Embarazada-con niño en brazos</option>
+                    <option value="Persona en condicion de Discapacidad">Persona en condición de Discapacidad</option>
+                </select>
+                
+                        
                 <input type="submit" value="Ok" />
             </form>
         </div>
