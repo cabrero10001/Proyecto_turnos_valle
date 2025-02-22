42081eca5e48c25698f03b9a49ca66f96901d4b2
Albarracin-sg <camilinplin2024@gmail.com>
2025-02-21 20:10:24 -0500

    modificacion de mi rama

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,15 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import Index from '../src/componentes/form/index'
+import "./index.css";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Pruebas de pagina</h1>
-      <p>Prueba para repositorio</p>
-      <h2>hola</h2>
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis temporibus a explicabo, reprehenderit harum animi cupiditate corporis nihil eveniet culpa aut nesciunt voluptatibus maiores aperiam asperiores veniam. Rem, modi placeat.</p>
+        <Index />
     </>
   )
 }
