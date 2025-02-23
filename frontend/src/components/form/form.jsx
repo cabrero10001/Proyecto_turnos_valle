5ac89e49a0f5dc08a8a86cb6c6ee06b20b28a4f2
Esteban <estivan2309@gmail.com>
2025-02-23 13:46:17 -0500

    modifique el tamaño para que se adapte a celular y puse para que saliera en distinto puerto cada pagina

--- frontend/src/components/form/form.jsx
+++ frontend/src/components/form/form.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import ipsLogo from "../../images/ips.png";
+import { Link } from "react-router-dom";
 
 /**
  * Componente funcional Form
@@ -15,6 +16,7 @@ const Form = () => {
     document: "",
     condition: "",
   });
+
   const handleInputChange = (event) => {
     const { name, value } = event.target; //se desestructura el event.target el cual acumula el name,value y type de los input
     setValues({
@@ -23,6 +25,7 @@ const Form = () => {
       [name]: value, //se guarda en 'value' el name y value del input modificado
     });
   };
+
   const handleSubmit = (event) => {
     event.preventDefault(); // Evita que la página se recargue
     console.log(values); // Muestra los datos en la consola
@@ -32,22 +35,24 @@ const Form = () => {
     <div className="relative min-h-screen w-screen bg-gradient-to-l from-[#E1EAF0] via-[#4187B5] to-[#2F688D] overflow-y-auto">
       {/* imagen de la ips */}
       <div className="absolute top-0 left-0 m-4">
-        <img src={ipsLogo} alt="IPS Logo" className="w-30 h-15" />
+        <img
+          src={ipsLogo}
+          alt="IPS Logo"
+          className="w-20 h-10 md:w-30 md:h-15" // Manteniendo el tamaño original
+        />
       </div>
 
       {/* Contenedor del formulario con estilos de fondo, bordes y espaciado */}
       <form
-        className="bg-[#d9d9d9] border-[20px] border-[#3c3c3c] p-[40px_50px_0px_50px] w-[500px] h-[600px] mx-auto mt-[50px] rounded-[0]"
+        className="bg-[#d9d9d9] border-[20px] border-[#3c3c3c] p-5 md:p-[40px_50px_0px_50px] w-[90%] max-w-[500px] h-auto md:h-[600px] mx-auto mt-[80px] rounded-[0]" // Manteniendo el tamaño original
         onSubmit={handleSubmit}
       >
-        {/* Contenedor adicional para el formulario (coso negro)*/}
-        <div className="absolute w-[192px] h-[60px] left-1/2 top-[40px] bg-[#1E1E1E] rounded-[10px] -translate-x-1/2"></div>
-
+        {/* Contenedor adicional para el formulario (coso negro) */}
+        <div className="absolute w-[150px] h-[50px] md:w-[192px] md:h-[60px] left-1/2 top-[20px] md:top-[30px] mt-[50px] bg-[#1E1E1E] rounded-[10px] -translate-x-1/2"></div>{" "}
         {/* Título del formulario */}
-        <h1 className="text-center text-[27px] mb-[18px] font-extrabold leading-[30px] tracking-[5px] whitespace-pre-line">
+        <h1 className="text-center text-[20.4px] md:text-[23.1px] mb-[15.3px] font-extrabold leading-[25.5px] tracking-[4.25px] whitespace-pre-line">
           FORMULARIO {"\n"}DE{"\n"} REGISTRO
         </h1>
-
         {/* Campo para el nombre */}
         <label htmlFor="name" className="font-bold">
           Nombre
@@ -55,17 +60,17 @@ const Form = () => {
         <input
           type="text"
           name="name"
-          className="text-[14px] w-full box-border p-[10px_12px] mb-[15px] bg-none border-b-[3px] border-b-[#6EA3C7]"
+          className="text-[10.2px] w-full box-border p-[7.65px_9px] mb-[10.8px] bg-none border-b-[2.175px] border-b-[#6EA3C7]" // Reducido en un 15%
           onChange={handleInputChange}
         />
         {/* Campo para el TIPO DE DOCUMENTO */}
-        <label htmlFor="condition" className="font-bold">
+        <label htmlFor="typeDocument" className="font-bold">
           Tipo de Documento
         </label>
         <input
           list="typeDocument"
           name="typeDocument"
-          className="text-[14px] w-full box-border bg-[#fff] p-[8px_12px] mb-[15px] mt-[10px]"
+          className="text-[10.2px] w-full box-border bg-[#fff] p-[5.78px_9px] mb-[10.8px] mt-[7.225px]" // Reducido en un 15%
           onChange={handleInputChange}
           placeholder="C.C"
         />
@@ -84,10 +89,9 @@ const Form = () => {
         <input
           type="number"
           name="document"
-          className="text-[14px] w-full box-border p-[10px_12px] mb-[15px] bg-none border-b-[3px] border-b-[#6EA3C7]"
+          className="text-[10.2px] w-full box-border p-[7.65px_9px] mb-[10.8px] bg-none border-b-[2.175px] border-b-[#6EA3C7]" // Reducido en un 15%
           onChange={handleInputChange}
         />
-
         {/* Campo para la condición con una lista de opciones */}
         <label htmlFor="condition" className="font-bold">
           Condición
@@ -95,7 +99,7 @@ const Form = () => {
         <input
           list="conditions"
           name="condition"
-          className="text-[14px] w-full box-border bg-[#fff] p-[8px_12px] mb-[15px] mt-[10px]"
+          className="text-[10.2px] w-full box-border bg-[#fff] p-[5.78px_9px] mb-[10.8px] mt-[7.225px]" // Reducido en un 15%
           onChange={handleInputChange}
           placeholder="N/A"
         />
@@ -105,11 +109,10 @@ const Form = () => {
           <option value="Mujer Embarazada"></option>
           <option value="Condición de Discapacidad"></option>
         </datalist>
-
         {/* Botón de envío del formulario */}
         <button
           type="submit"
-          className="bg-[#6EA3C7] font-bold w-[150px] h-[50px] text-white py-3 mt-0 rounded-lg hover:bg-[#3c3c3c] transition-colors mx-auto block"
+          className="bg-[#6EA3C7] font-bold w-[108.375px] h-[36.125px] text-white py-[2.175px] mt-0 rounded-lg hover:bg-[#3c3c3c] transition-colors mx-auto block" // Reducido en un 15%
         >
           ENVIAR
         </button>
