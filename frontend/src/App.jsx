f5004c2ffa64225ec181503367619a0d58a00079
Albarracin-sg <camilinplin2024@gmail.com>
2025-02-23 01:50:06 -0500

    eliminacion de unas importaciones de por defecto en app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import '../src/index.css';
 import Form from './components/form/form';
 import Ticket from './components/ticket/ticket';
