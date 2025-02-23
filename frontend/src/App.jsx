7cf707dd14bc2deab0e8268121889b8c1549e85f
Esteban <estivan2309@gmail.com>
2025-02-22 22:20:01 -0500

    Mejore la forma de Ticket y modifique algunos apartados mas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { useState } from "react";
+import Body from "./components/body.jsx";
+import "./index.css";
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <h1>Pruebas de pagina</h1>
-      <p>Prueba para repositorio</p>
+      <Body />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
