7efb91d47f3c1a26d46e93d6f40c8ff16e757a01
Jajavier2404 <javialex2008@gmail.com>
2025-02-23 17:29:21 -0500

    no importa este commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import '../src/index.css';
 import Form from './components/form/form';
-import Ticket from './components/ticket/ticket';
+
 
 function App() {
   return (
     <>
+      
       <Form />
-      <Ticket />
       </>
     
   )
