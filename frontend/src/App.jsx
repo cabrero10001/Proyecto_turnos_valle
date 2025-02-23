ad84da0ad6c48c2fca39bd4491d5d7969e835ce2
Albarracin-sg <camilinplin2024@gmail.com>
2025-02-23 16:41:36 -0500

    rutas de paginas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,28 @@
+// Importa los estilos globales de la aplicación desde index.css
 import '../src/index.css';
+
+// Importa los componentes Form y Ticket desde sus respectivas rutas
 import Form from './components/form/form';
 import Ticket from './components/ticket/ticket';
 
+// Importa las herramientas necesarias de React Router para manejar la navegación
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+// Componente principal de la aplicación
 function App() {
   return (
-    <>
-      <Form />
-      <Ticket />
-      </>
-    
-  )
+    // Envuelve la aplicación en BrowserRouter para habilitar la funcionalidad de enrutamiento
+    <BrowserRouter>
+      <Routes>
+        {/* Define la ruta principal ("/") y renderiza el componente Form */}
+        <Route path="/" element={<Form />} />
+
+        {/* Define la ruta "/ticket" y renderiza el componente Ticket */}
+        <Route path="/ticket" element={<Ticket />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
+// Exporta el componente App para que pueda ser usado en otros archivos
 export default App;
